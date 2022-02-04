9c13896aa9fd46ca07ad528b7c4c5acaa692f5e3
marcusruas <marcus.ruas@hotmail.com>
2022-02-03 22:10:06 -0300

    Criado projeto de logs

---
+++ MandradeFrameworks.Logs/Configuration/LogsConfiguration.cs
@@ -0,0 +1,89 @@
+﻿using MandradeFrameworks.Logs.Models;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.SqlClient;
+using Dapper;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System.Collections.ObjectModel;
+
+namespace MandradeFrameworks.Logs.Configuration
+{
+    public static class LogsConfiguration
+    {
+        /// <summary>
+        /// Método para adicionar logs na aplicação.
+        /// </summary>
+        public static void AdicionarLogs(this IServiceCollection services, LogsOpcoesSQL opcoes)
+        {
+            if (string.IsNullOrWhiteSpace(opcoes.ConnectionString))
+                throw new ArgumentException("Connection String da base de logs não pode ser vazio.");
+
+            if (string.IsNullOrWhiteSpace(opcoes.Tabela))
+                throw new ArgumentException("Nome da tabela de logs não pode ser vazio.");
+
+            services.AddSingleton(opcoes);
+
+            GerarTabelaSQL(opcoes);
+            CriarInstanciaSerilog(opcoes);
+        }
+        private static void CriarInstanciaSerilog(LogsOpcoesSQL opcoes)
+        {
+            string schema = string.IsNullOrWhiteSpace(opcoes.Schema) ? "dbo" : opcoes.Schema;
+
+            var additionalColumns = new Collection<SqlColumn> { new SqlColumn { ColumnName = "Action" } };
+            var options = new ColumnOptions() { AdditionalColumns = additionalColumns };
+            
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .MSSqlServer(
+                    connectionString: opcoes.ConnectionString,
+                    sinkOptions: new MSSqlServerSinkOptions { SchemaName = schema, TableName = opcoes.Tabela },
+                    columnOptions: options
+                )
+                .CreateLogger();
+        }
+
+        private static void GerarTabelaSQL(LogsOpcoesSQL opcoes)
+        {
+            var consulta = QueryCriacaoTabela(opcoes);
+
+            try
+            {
+                using (var connection = new SqlConnection(opcoes.ConnectionString))
+                    connection.Execute(consulta);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Ocorreu uma falha ao realizar o cadastro da tabela SQL. Detalhes: {ex}");
+            }
+        }
+
+        private static string QueryCriacaoTabela(LogsOpcoesSQL opcoes)
+        {
+            string schema = string.IsNullOrWhiteSpace(opcoes.Schema) ? "dbo" : opcoes.Schema;
+            string tabela = $"[{schema}].[{opcoes.Tabela}]";
+
+            return $@"
+                IF NOT EXISTS(OBJECT_ID('{tabela}'))
+                BEGIN
+                    CREATE TABLE [Logs] (
+                       [Id] int IDENTITY(1,1) NOT NULL,
+                       [Message] nvarchar(500) NULL,
+                       [MessageTemplate] nvarchar(500) NULL,
+                       [Level] nvarchar(128) NULL,
+                       [TimeStamp] datetime NOT NULL,
+                       [Exception] nvarchar(max) NULL,
+                       [Properties] nvarchar(max) NULL,
+                       [Action] nvarchar(max) NULL
+
+                       CONSTRAINT [PK_Logs] PRIMARY KEY CLUSTERED ([Id] ASC) 
+                    );
+
+                    CREATE INDEX IX_Consulta_Simplificada ON {tabela} (Data, Message)
+                    CREATE INDEX IX_Consulta_Nivel ON {tabela} (Data, Level)
+                END
+            ";
+        }
+    }
+}
---
+++ MandradeFrameworks.Logs/Models/LogsOpcoesSQL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MandradeFrameworks.Logs.Models
+{
+    public class LogsOpcoesSQL
+    {
+        public LogsOpcoesSQL(string connectionString, string tabela, string schema = null, bool logsHabilitados = true)
+        {
+            ConnectionString = connectionString;
+            Schema = schema;
+            Tabela = tabela;
+            LogsHabilitados = logsHabilitados;
+        }
+
+        /// <summary>
+        /// Connection String do banco SQL Server onde será armazenado a tabela de logs.
+        /// </summary>
+        public string ConnectionString { get; set; }
+        /// <summary>
+        /// Schema da tabela que irá gravar os logs da aplicação. caso não haja schema, pode ser passado null.
+        /// </summary>
+        public string Schema { get; set; }
+        /// <summary>
+        /// Nome da tabela que irá gravar os logs da aplicação.
+        /// </summary>
+        public string Tabela { get; }
+        /// <summary>
+        /// Informa se a aplicação está habilitada para logs.
+        /// </summary>
+        public bool LogsHabilitados { get; }
+    }
+}
