4540f077b7c67577ea8e6e2b1706ed7f84e501cf
marcusruas <marcus.ruas@hotmail.com>
2022-03-20 00:10:37 -0300

    Corrigido ctor da specification paginates

--- MandradeFrameworks.SharedKernel/Models/BaseSpecificationPaginated.cs
+++ MandradeFrameworks.SharedKernel/Models/BaseSpecificationPaginated.cs
@@ -7,24 +7,19 @@ namespace MandradeFrameworks.SharedKernel.Models
 {
     public abstract class BaseSpecificationPaginated<T> : BaseSpecification<T>
     {
-        /// <summary>
-        /// Página a ser pesquisada
-        /// </summary>
-        public int Pagina { get; private set; }
-        /// <summary>
-        /// Quantidade de registros da página
-        /// </summary>
-        public int QuantidadeRegistros { get; private set; }
-
-        /// <summary>
-        /// Adiciona paginação a query de specification
-        /// </summary>
-        /// <param name="pagina">Página a ser pesquisada</param>
-        /// <param name="quantidadeRegistros">Quantidade de registros por página</param>
-        public void AdicionarPaginacao(int pagina, int quantidadeRegistros)
+        public BaseSpecificationPaginated(int pagina, int quantidadeRegistros)
         {
             Pagina = pagina;
             QuantidadeRegistros = quantidadeRegistros;
         }
+
+        /// <summary>
+        /// Página a ser pesquisada
+        /// </summary>
+        public int Pagina { get; }
+        /// <summary>
+        /// Quantidade de registros da página
+        /// </summary>
+        public int QuantidadeRegistros { get; }
     }
 }
